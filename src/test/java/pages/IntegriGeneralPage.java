8797336c0da842bbed1cc9257e3eaaaab4a73f78
GorbachL <52042279+GorbachL@users.noreply.github.com>
2020-01-21 18:14:16 +0300

    Added General page

--- src/test/java/pages/IntegriGeneralPage.java
+++ src/test/java/pages/IntegriGeneralPage.java
@@ -1,8 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class IntegriGeneralPage extends BasePage {
+
+    public static final String URL = "https://dev.integrivideo.com/login";
+    public static final By LOG_IN_BUTTON = By.xpath("//button[text() = 'Log in']");
+    public static final By LOG_IN_LINK = By.xpath("//*[text() = 'Log in']");
+    public static final By SIGN_UP_LINK = By.xpath("//*[text() = 'Sign up']");
+    public static final By SIGN_UP_BUTTON = By.xpath("//button[text() = 'Sign up']");
+    public static final By FORGOT_LINK = By.cssSelector("a.forgot");
+
 
     public IntegriGeneralPage(WebDriver driver) {
         super(driver);
